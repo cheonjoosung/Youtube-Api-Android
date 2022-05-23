c989b779534616f88f9e63911fcffe18807f5256
cheonjoosung <cjswntjd@gmail.com>
2022-05-23 10:49:52 +0900

    검색 레이아웃 추가

--- app/src/main/java/kr/co/js/youtube_search/ui/MainActivity.kt
+++ app/src/main/java/kr/co/js/youtube_search/ui/MainActivity.kt
@@ -1,12 +1,57 @@
 package kr.co.js.youtube_search.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import kr.co.js.youtube_search.R
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.js.youtube_search.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            ivSearch.setOnClickListener {
+                searchYoutube(binding.etSearch.text.toString())
+            }
+
+            etSearch.setOnEditorActionListener { _, id, keyEvent ->
+                if (id == EditorInfo.IME_ACTION_SEARCH) {
+                    searchYoutube(etSearch.text.toString())
+                }
+
+                true
+            }
+        }
+    }
+
+    private fun searchYoutube(keyword: String) {
+        if (keyword.isEmpty()) {
+            Toast.makeText(applicationContext, "Search Keyword Empty!!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        hideKeyboard()
+
+        Log.e(TAG, "keywrod $keyword")
+    }
+
+    private fun hideKeyboard() {
+        binding.etSearch.clearFocus()
+        val inputMethodManager = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
     }
 }
