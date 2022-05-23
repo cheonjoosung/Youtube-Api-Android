f0c6f54ab79ac8845fe31e56bc3178b3ec77a1e6
cheonjoosung <cjswntjd@gmail.com>
2022-05-23 10:50:09 +0900

    유튜브 플레이어 액티비티 추가

---
+++ app/src/main/java/kr/co/js/youtube_search/ui/YoutubePlayerActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.js.youtube_search.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.js.youtube_search.R
+
+class YoutubePlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube_player)
+    }
+}
