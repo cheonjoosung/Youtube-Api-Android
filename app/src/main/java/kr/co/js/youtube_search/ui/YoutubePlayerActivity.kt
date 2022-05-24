0f46998c6665d73a4409c824668b0fdd66e2ecdd
cheonjoosung <cjswntjd@gmail.com>
2022-05-24 11:13:24 +0900

    유튜브 플레이어 화면 및 로직 추가

--- app/src/main/java/kr/co/js/youtube_search/ui/YoutubePlayerActivity.kt
+++ app/src/main/java/kr/co/js/youtube_search/ui/YoutubePlayerActivity.kt
@@ -1,12 +1,13 @@
 package kr.co.js.youtube_search.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kr.co.js.youtube_search.R
-import kr.co.js.youtube_search.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import kr.co.js.youtube_search.databinding.ActivityYoutubePlayerBinding
 import kr.co.js.youtube_search.model.vo.Video
+import kr.co.js.youtube_search.util.VideoUtil
 
 class YoutubePlayerActivity : AppCompatActivity() {
 
@@ -17,13 +18,33 @@ class YoutubePlayerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityYoutubePlayerBinding
 
+    private val utils = VideoUtil()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityYoutubePlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val data = intent.getParcelableExtra<Video>(VIDEO)
-        Log.e(TAG, "data is $data")
+        val video = intent.getParcelableExtra<Video>(VIDEO)
+        Log.e(TAG, "data is $video")
+
+        binding.apply {
+            lifecycle.addObserver(youtubePlayerView)
+
+            youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    val videoId = video?.videoId ?: ""
+                    youTubePlayer.loadVideo(videoId, 0f)
+                }
+            })
+
+            tvVideoTitle.text = video?.title
+            tvVideoChannelName.text = video?.channelTitle
+            tvVideoDate.text = utils.convertPublishedDate(video?.publishedAt ?: "")
+            tvVideoViewCount.text = utils.convertViewCount(video?.viewCount ?: "")
+            tvVideoDuration.text = utils.convertDurationToHHMMSS(video?.duration ?: "")
+            tvVideoDescription.text = video?.description
+        }
     }
 }
