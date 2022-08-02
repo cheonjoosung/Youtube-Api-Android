33381b63722b14a4763610db65bb7d6baa839e26
cheonjoosung <cjswntjd@gmail.com>
2022-08-02 20:59:54 +0900

    Hilt 애플리케이션 클래스 추가(코드 생성 트리거)

--- app/src/main/java/kr/co/js/youtube_search/ui/VideoApplication.kt
+++ app/src/main/java/kr/co/js/youtube_search/ui/VideoApplication.kt
@@ -1,8 +1,10 @@
 package kr.co.js.youtube_search.ui
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import kr.co.js.youtube_search.model.repository.SearchRepository
 
+@HiltAndroidApp
 class VideoApplication : Application() {
     val searchRepository by lazy { SearchRepository() }
 }
