ec25e2d772330536fed7bf715c5f217688c5d487
cheonjoosung <cjswntjd@gmail.com>
2022-08-13 23:58:06 +0900

    [클린아키텍쳐 도입] - 의존성 관리 파일 추가

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,49 @@
+object AndroidX {
+    const val CORE_KTX = "androidx.core:core-ktx:1.8.0"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.5.0"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val LEGACY_SUPPORT = "androidx.legacy:legacy-support-v4:1.0.0"
+    const val PREFERENCE = "androidx.preference:preference:1.2.0"
+    const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:2.5.1"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:2.5.1"
+
+    const val VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1"
+    const val LIVE_DATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.5.1"
+    const val COMMON_JAVA = "androidx.lifecycle:lifecycle-common-java8:2.5.1"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.6.1"
+}
+
+object Youtube {
+    const val YOUTUBE_WEB_PLAYER = "com.pierfrancescosoffritti.androidyoutubeplayer:core:11.0.1"
+}
+
+object Retrofit {
+    const val RETROFIT2 = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val GSON_CONVERTOR = "com.squareup.retrofit2:converter-gson:2.9.0"
+    const val ADAPTER_RXJAVA = "com.squareup.retrofit2:adapter-rxjava2:2.9.0"
+}
+
+object OkHttp {
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:4.9.1"
+    const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:4.9.2"
+}
+
+object DaggerHilt {
+    const val DAGGER_HILT = "com.google.dagger:hilt-android:2.40.5"
+    const val DAGGER_HILT_COMPILER = "com.google.dagger:hilt-android-compiler:2.40.5"
+    const val DAGGER_HILT_VIEW_MODEL = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
+    const val DAGGER_HILT_ANDROIDX_COMPILER = "androidx.hilt:hilt-compiler:1.0.0"
+}
+
+object Glide {
+    const val GLIDE = "com.github.bumptech.glide:glide:4.11.0"
+    const val GLIDE_COMPILER = "com.github.bumptech.glide:compiler:4.11.0"
+}
+
+object RxJava {
+    const val RX_ANDROID = "io.reactivex.rxjava2:rxandroid:2.1.1"
+    const val RX_KOTLIN = "io.reactivex.rxjava2:rxkotlin:2.4.0"
+}
