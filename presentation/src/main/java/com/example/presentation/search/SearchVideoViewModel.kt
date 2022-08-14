3d6f0dad50e9c217665d46d2c642425230da8c32
cheonjoosung <cjswntjd@gmail.com>
2022-08-15 01:41:07 +0900

    [클린아키텍쳐 도입] - app 모듈 > 미사용코드 제거

--- presentation/src/main/java/com/example/presentation/search/SearchVideoViewModel.kt
+++ presentation/src/main/java/com/example/presentation/search/SearchVideoViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.ApiResult
 import com.example.domain.model.Video
-import com.example.domain.model.VideoResult
 import com.example.domain.usecase.SearchVideoUseCase
 import com.example.domain.usecase.VideoInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -149,84 +148,6 @@ class SearchVideoViewModel @Inject constructor(
                 }
             }
         }
-
-        /*val result = searchRepository.getYouTubeMoreVideos(savedKeyword, nextPageToken)
-
-        when (result.code()) {
-            200 -> {
-
-                /**
-                 * Youtube Search API Do not return viewCount & Duration
-                 * So, Video API needed after Search API
-                 * 검색 API는 조회수&영상길이를 주지 않으므로 비디오 API 를 통해 조회가 필요함
-                 */
-                val idList = mutableListOf<String>()
-                val convertedList = mutableListOf<Video>()
-
-                result.body()?.let {
-
-                    nextPageToken = it.nextPageToken
-
-                    it.items?.let { list ->
-                        for (item in list) {
-
-                            val id = item.id
-                            val snippet = item.snippet
-
-                            convertedList.add(
-                                Video(
-                                    videoId = id.videoId,
-                                    title = snippet.title,
-                                    description = snippet.description,
-                                    publishedAt = snippet.publishedAt,
-                                    imgUrl = snippet.thumbnails.medium.url,
-                                    channelTitle = snippet.channelTitle
-                                )
-                            )
-
-                            idList.add(id.videoId)
-                        }
-                    }
-
-                }
-
-                coroutineScope {
-                    (0 until idList.size).map { idx ->
-                        async(Dispatchers.IO) {
-                            val resultInfo = searchRepository.requestVideoInfo(idList[idx])
-
-                            when (resultInfo.code()) {
-                                200 -> {
-                                    resultInfo.body()?.let {
-                                        convertedList.find { video ->
-                                            video.videoId == idList[idx]
-                                        }?.let { findVideo ->
-                                            it.items?.let { list ->
-                                                for (item in list) {
-                                                    findVideo.duration =
-                                                        item.contentDetails.duration
-                                                    findVideo.viewCount =
-                                                        item.statistics.viewCount.toString()
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                                else -> {
-                                    Log.e(TAG, "onFailure ${resultInfo.message()}")
-                                }
-                            }
-                        }
-                    }.awaitAll()
-
-                    moreSearch.postValue(convertedList)
-                }
-
-            }
-            else -> {
-                Log.e(TAG, "onFailure ${result.message()}")
-            }
-        }*/
 
     }
 }
